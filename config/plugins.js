add8b8da5a4119a0e05a9ec44634f4824d57fa3d
Ivaylo Ovcharov <entropyactive@gmail.com>
2021-06-13 21:28:18 +0300

    update cloudinary

--- config/plugins.js
+++ config/plugins.js
@@ -1,15 +1,12 @@
 module.exports = ({ env }) => ({
-    // ...
-    upload: {
-      provider: 'aws-s3',
-      providerOptions: {
-        accessKeyId: env('AWS_ACCESS_KEY_ID'),
-        secretAccessKey: env('AWS_ACCESS_SECRET'),
-        region: env('AWS_REGION'),
-        params: {
-          Bucket: env('AWS_BUCKET'),
-        },
-      },
-    },
-    // ...
-  });
+      upload: {
+        provider: 'cloudinary',
+        providerOptions: {
+        cloud_name: env('CLOUDINARY_CLOUD_NAME'),
+        api_key: env('CLOUDINARY_API_KEY'),
+        api_secret: env('CLOUDINARY_API_SECRET'),
+        secure: true,
+        upload_preset: env('CLOUDINARY_UPLOAD_PRESET')
+      }
+    }
+  })
